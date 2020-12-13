f58696567d206c1435f6d3b6fdfb94e57f74d510
Siyuan <suquark@gmail.com>
2020-12-12 22:12:17 -0800

    fix

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -32,15 +32,18 @@ class RADSimCLR(nn.Module):
 
 class ResNetSimCLR(nn.Module):
 
-    def __init__(self, base_model, out_dim):
+    def __init__(self, base_model, out_dim, obs_shape=(9, 84, 84)):
         super(ResNetSimCLR, self).__init__()
         self.resnet_dict = {"resnet18": models.resnet18(pretrained=False),
                             "resnet50": models.resnet50(pretrained=False)}
 
         resnet = self._get_basemodel(base_model)
         num_ftrs = resnet.fc.in_features
-
-        self.features = nn.Sequential(*list(resnet.children())[:-1])
+        layers = list(resnet.children())[:-1]
+        layers[0] = nn.Conv2d(obs_shape[0], self.inplanes, kernel_size=7, stride=2, padding=3,
+                              bias=False)
+        nn.init.kaiming_normal_(layers[0].weight, mode='fan_out', nonlinearity='relu')
+        self.features = nn.Sequential(*layers)
 
         # projection MLP
         self.l1 = nn.Linear(num_ftrs, num_ftrs)
